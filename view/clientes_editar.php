fa1758226336d92d8bc9c8a5141e558468db7469
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2021-03-25 19:22:45 -0300

    Correção dos nomes do formulário

--- view/clientes_editar.php
+++ view/clientes_editar.php
@@ -19,44 +19,44 @@
 <div class="card-body">
 <?php
 $resultado = $cliente->get_cliente($cpf);
-//print_r($resultado);
 ?>
 <form action="<?php echo ENDERECO; ?>/clientes/atualizar/08416200670" method="post">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputNome">Nome</label>
       <input type="text" class="form-control" id="inputNome" name="nome" value="<?php echo $resultado["nome"]; ?>" maxlength="100">
+      
     </div>
     <div class="form-group col-md-6">
-      <label for="inputPassword4">E-mail</label>
-      <input type="text" class="form-control" id="inputPassword4" name="email" value="<?php echo $resultado["email"]; ?>" maxlength="100">
-    </div>
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputNome">CPF</label>
-      <input type="text" class="form-control" id="inputNome" name="cpf" value="<?php echo $resultado["cpf"]; ?>" readonly>
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Data de Nascimento</label>
-      <input type="date" class="form-control" id="inputPassword4" name="data_de_nascimento" value="<?php echo $resultado["data_de_nascimento"]; ?>">
+      <label for="inputEmail">E-mail</label>
+      <input type="text" class="form-control" id="inputEmail" name="email" value="<?php echo $resultado["email"]; ?>" maxlength="100">
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputNome">Sexo</label>
-        <select name=sexo class="form-control">
-          <option value='Masculino' <?php if($resultado["sexo_cliente"] == 'Masculino') { echo 'selected'; } ?>>Masculino</option>
-          <option value='Feminino' <?php if($resultado["sexo_cliente"] == 'Feminino') { echo 'selected'; } ?>>Feminino</option>
+      <label for="inputCPF">CPF</label>
+      <input type="text" class="form-control" id="inputCPF" name="cpf" value="<?php echo $resultado["cpf"]; ?>" readonly>
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputDataDeNascimento">Data de Nascimento</label>
+      <input type="date" class="form-control" id="inputDataDeNascimento" name="data_de_nascimento" value="<?php echo $resultado["data_de_nascimento"]; ?>">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputSexo">Sexo</label>
+        <select name="sexo" id="selectSexo" class="form-control">
+          <option value='Masculino' name='Masculino'  <?php if($resultado["sexo_cliente"] == 'Masculino') { echo 'selected'; } ?>>Masculino</option>
+          <option value='Feminino' name='Feminino' <?php if($resultado["sexo_cliente"] == 'Feminino') { echo 'selected'; } ?>>Feminino</option>
         </select>
     </div>
     <div class="form-group col-md-6">
-        <label for="inputNome">Estado Civil</label>
-            <select name=estado_civil class="form-control">
-            <option value=Solteiro <?php if($resultado["estado_civil_cliente"] == 'Solteiro') { echo 'selected'; } ?>>Solteiro</option>
-                <option value=Casado <?php if($resultado["estado_civil_cliente"] == 'Casado') { echo 'selected'; } ?>>Casado</option>
-                <option value=Divorciado <?php if($resultado["estado_civil_cliente"] == 'Divorciado') { echo 'selected'; } ?>>Divorciado</option>
-                <option value=Viúvo <?php if($resultado["estado_civil_cliente"] == 'Viúvo') { echo 'selected'; } ?>>Viúvo</option>
+        <label for="inputEstadoCivil">Estado Civil</label>
+            <select id="inputEstadoCivil" name='estado_civil' class="form-control">
+            <option value="Solteiro" name='Solteiro' <?php if($resultado["estado_civil_cliente"] == 'Solteiro') { echo 'selected'; } ?>>Solteiro</option>
+                <option value="Casado" name='Casado' <?php if($resultado["estado_civil_cliente"] == 'Casado') { echo 'selected'; } ?>>Casado</option>
+                <option value="Divorciado" name='Divorciado' <?php if($resultado["estado_civil_cliente"] == 'Divorciado') { echo 'selected'; } ?>>Divorciado</option>
+                <option value="Viúvo" name='Viúvo' <?php if($resultado["estado_civil_cliente"] == 'Viúvo') { echo 'selected'; } ?>>Viúvo</option>
             </select>
     </div>
   </div>
