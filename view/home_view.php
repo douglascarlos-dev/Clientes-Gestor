a31906e585597470edb2f45a90af676377a1e93f
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2022-05-25 22:02:51 -0300

    :recycle: Refatoração do código

--- view/home_view.php
+++ view/home_view.php
@@ -8,15 +8,15 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.min.css" crossorigin="anonymous">
 
-    <title>Sistema 1.0</title>
+    <title>Clientes Gestor 1.0</title>
   </head>
   <body>
   <?php require_once 'menu.php'; ?>
   <div id="dashboard">
-    <p>&emsp;Clientes Gestor</p>
-    <p>&emsp;Esse projeto faz parte dos meus estudos sobre programação e teste de software.</p>
-    <p>&emsp;Software Web para cadastro de clientes. No seu desenvolvimento foi utlizado URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO.</p>
-    <p>&emsp;Para mais informações, acesse o site <a href="https://github.com/douglascarlos-dev/Clientes-Gestor" target="_blank">https://github.com/douglascarlos-dev/Clientes-Gestor</a>.</p>
+    <p class="px-md-4">Clientes Gestor</p>
+    <p class="px-md-4">Esse projeto faz parte dos meus estudos sobre programação e teste de software.</p>
+    <p class="px-md-4">Software Web para cadastro de clientes. No seu desenvolvimento foi utlizado URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO.</p>
+    <p class="px-md-4">Para mais informações, acesse o site <a href="https://github.com/douglascarlos-dev/Clientes-Gestor" target="_blank">https://github.com/douglascarlos-dev/Clientes-Gestor</a>.</p>
   </div>
 <script type="text/javascript" src="<?php echo URLROOT; ?>/js/jquery-3.5.1.slim.min.js"></script>
 <script src="<?php echo URLROOT; ?>/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
--- view/user_login.php
+++ view/user_login.php
@@ -11,49 +11,36 @@
     <title>Clientes Gestor</title>
     <meta property="og:image" content="https://douglascarlos.dev/clientesgestordemo/img/5138237.png"/>
     <meta property="og:description" content="Software Web para cadastro de clientes com URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO"/>
-
   </head>
   <body>
   <div class="mx-auto text-center col-md-8 order-md-1">
-  <img src="<?php echo URLROOT; ?>/img/5138237.svg" height="200" width="274" alt="Contato">
-  <h4>Clientes Gestor 1.0</h4>
-  <br>
-  Demonstração:
-  <br><br>
-  Usuário: admin
-  <br>
-  Senha: admin
-  <br><br>
-
-  <div class="card">
-    
-  <div class="card-body">
-
-  
-
-  
-
-  <form name="entrar" action="<?php echo URLROOT; ?>/user/login" method="post">
-
-  <div class="form-row">
-  <div class="form-group col-md-6">
-      <label for="inputUsuario">Usuário</label>
-      <input type="text" class="form-control" id="inputUsuario" name="username" autocomplete="username" placeholder="Usuário" maxlength="100">
+    <img src="<?php echo URLROOT; ?>/img/5138237.svg" height="200" width="274" alt="Contato">
+    <h4>Clientes Gestor 1.0</h4>
+    <br>
+    Demonstração:
+    <br><br>
+    Usuário: admin
+    <br>
+    Senha: admin
+    <br><br>
+    <div class="card">
+      <div class="card-body">
+        <form name="entrar" action="<?php echo URLROOT; ?>/user/login" method="post">
+          <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputUsuario">Usuário</label>
+                <input type="text" class="form-control" id="inputUsuario" name="username" autocomplete="username" placeholder="Usuário" maxlength="100">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputSenha">Senha</label>
+                <input type="password" class="form-control" id="inputSenha" name="password" autocomplete="off" placeholder="Senha" maxlength="100">
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+      </div>
+    </div>
   </div>
-
-  <div class="form-group col-md-6">
-      <label for="inputSenha">Senha</label>
-      <input type="password" class="form-control" id="inputSenha" name="password" autocomplete="off" placeholder="Senha" maxlength="100">
-  </div>
-  </div>
-
-  <button type="submit" class="btn btn-primary">Entrar</button>
-  </form>
-
-  </div>
-  </div>
-  </div>
-
 <script type="text/javascript" src="<?php echo URLROOT; ?>/js/jquery-3.5.1.slim.min.js"></script>
 <script src="<?php echo URLROOT; ?>/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
