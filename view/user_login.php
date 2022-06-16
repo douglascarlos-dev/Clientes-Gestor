e35030b54da3f202239770deedc13f60d29ec1d0
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2022-06-16 11:35:42 -0300

    :zap: Atualizações gerais

--- view/user_login.php
+++ view/user_login.php
@@ -4,12 +4,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Clientes Gestor - Software Web para cadastro de clientes com URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO">
-    <meta name="author" content="Douglas">
+    <meta name="description" content="Software Web para cadastro de clientes com URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO">
+    <meta name="author" content="Douglas Carlos">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo URLROOT ?>/css/bootstrap.min.css" crossorigin="anonymous">
     <title>Clientes Gestor</title>
     <meta property="og:image" content="https://douglascarlos.dev/clientesgestordemo/img/5138237.png"/>
+    <meta property="og:title" content="Clientes Gestor"/>
     <meta property="og:description" content="Software Web para cadastro de clientes com URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO"/>
   </head>
   <body>
