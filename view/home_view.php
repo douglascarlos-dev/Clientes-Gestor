6aef339a9c7978fe3e787f85d387f8dbe2736b02
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2022-08-01 20:45:22 -0300

    :zap: Atualizações gerais

--- view/home_view.php
+++ view/home_view.php
@@ -20,7 +20,7 @@
   <div id="dashboard" class="mobile-margin visible-xs">
     <p>Clientes Gestor</p>
     <p>Esse projeto faz parte dos meus estudos sobre programação e teste de software.</p>
-    <p>Software Web para cadastro de clientes. No seu desenvolvimento foi utlizado URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO.</p>
+    <p>Software Web para cadastro de clientes. No seu desenvolvimento foi utlizado URL amigável usando PHP Orientado a Objetos, MVC e banco de dados Postgresql com PDO e CAPTCHA.</p>
     <p>Para mais informações, acesse o site <a href="https://github.com/douglascarlos-dev/Clientes-Gestor" target="_blank">https://github.com/douglascarlos-dev/Clientes-Gestor</a>.</p>
   </div>
 <script type="text/javascript" src="<?php echo URLROOT; ?>/js/jquery-3.5.1.slim.min.js"></script>
