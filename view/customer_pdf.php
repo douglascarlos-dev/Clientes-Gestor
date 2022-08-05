988884f2aa5ae96298410cd4aacb79bde98cdea5
Douglas Carlos da Silva Oliveira <contato@douglascarlos.dev>
2022-08-05 20:48:40 -0300

    :pencil: Adição de biblioteca para geração de pdf

--- view/customer_pdf.php
+++ view/customer_pdf.php
@@ -14,16 +14,47 @@ $nome = $customer->getName();
 $email = $customer->getEmail();
 $cpf = Mask("###.###.###-##",$customer->getCPF());
 $data_de_nascimento = $customer->getBirthDate();
+$sexo = "";
+if($customer->getSex() == 'Masculino') { $sexo = 'Masculino'; }
+if($customer->getSex() == 'Feminino') { $sexo = 'Feminino'; }
+$estado_civil = "";
+if($customer->getMaritalStatus() == 'Solteiro') { $estado_civil = 'Solteiro'; }
+if($customer->getMaritalStatus() == 'Casado') { $estado_civil = 'Casado'; }
+if($customer->getMaritalStatus() == 'Divorciado') { $estado_civil = 'Divorciado'; }
+if($customer->getMaritalStatus() == 'Viúvo') { $estado_civil = 'Viúvo'; }
+$html_telefone = "";
+foreach($phone as &$phone_value):
+    $html_telefone .= "<tr>";
+    $html_telefone .= "<td><b>Telefone:</b> " . $phone_value->getType() . "</td>";
+    $phone_ = $phone_value->getPhone();
+    $html_telefone .= "<td><b>Número:</b> " . $phone_ . "</td>";
+    $html_telefone .= "</tr>";
+endforeach;
+$html_endereco = "";
+foreach($address as &$address_value):
+    $html_endereco .= "<br><tr>";
+    $html_endereco .= "<td><b>Categoria endereço:</b> " . $address_value->getAddressCategory() . "</td>";
+    $html_endereco .= "<td><b>Tipo:</b> " . $address_value->getType() . "</td>";
+    $html_endereco .= "</tr>";
+    $html_endereco .= "<tr>";
+    $html_endereco .= "<td><b>Nome:</b> " . $address_value->getName() . "</td>";
+    $html_endereco .= "<td><b>Número:</b> " . $address_value->getNumber() . "</td>";
+    $html_endereco .= "</td>";
+    $html_endereco .= "<tr>";
+    $html_endereco .= "<td><b>Bairro:</b> " . $address_value->getDistrict() . "</td>";
+    $html_endereco .= "<td><b>Cidade:</b> " . $address_value->getCity() . "</td>";
+    $html_endereco .= "</tr>";
+    $html_endereco .= "<tr>";
+    $html_endereco .= "<td><b>CEP:</b> " . $address_value->getZipCode() . "</td>";
+    $html_endereco .= "<td><b>UF:</b> " . $address_value->getState() . "</td>";
+    $html_endereco .= "</tr>";
+    $html_endereco .= "<tr>";
+    $html_endereco .= "<td colspan=2><b>Complemento:</b> " . $address_value->getComplement() . "</td>";
+    $html_endereco .= "</tr>";
+endforeach;
+
+
 /*
-
-
-
-if($customer->getSex() == 'Masculino') { echo 'selected'; }
-if($customer->getSex() == 'Feminino') { echo 'selected'; }
-if($customer->getMaritalStatus() == 'Solteiro') { echo 'selected'; }
-if($customer->getMaritalStatus() == 'Casado') { echo 'selected'; }
-if($customer->getMaritalStatus() == 'Divorciado') { echo 'selected'; }
-if($customer->getMaritalStatus() == 'Viúvo') { echo 'selected'; }
 */
 
 // include autoloader
@@ -35,7 +66,7 @@ use Dompdf\Dompdf;
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
 $dompdf->add_info('Title', 'Your meta title');
-$dompdf->loadHtml('<title>Relatório Cliente</title><meta name="author" content="https://douglascarlos.dev/clientesgestordemo/"><hr style=\'width: 90%; height:5px; text-align:center; border:0px; color:#ff9999; background:#ff9999;\' />
+$dompdf->loadHtml('<title>' . $nome . ' - Relatório Cliente</title><meta name="author" content="https://douglascarlos.dev/clientesgestordemo/"><hr style=\'width: 90%; height:5px; text-align:center; border:0px; color:#ff9999; background:#ff9999;\' />
 <center>Consulta realizada em: ' . $today . '<br>
 <h3>Clientes Gestor</h3>
 <h3>Relatório Cliente</h3></center><br>
@@ -48,6 +79,13 @@ $dompdf->loadHtml('<title>Relatório Cliente</title><meta name="author" content=
         <td><b>CPF:</b> ' . $cpf . '</td>
         <td><b>Data de Nascimento:</b> ' . $data_de_nascimento . '</td>
     </tr>
+    <tr>
+        <td><b>Sexo:</b> ' . $sexo . '</td>
+        <td><b>Estado Civil:</b> ' . $estado_civil . '</td>
+    </tr><br>
+    ' . $html_telefone . '
+    
+    ' . $html_endereco . '
 </table>');
 
 // (Optional) Setup the paper size and orientation
